dab3354b32afff117a7150148cd8b39c647d3af3
Cassandra Ganska <89425242+CasGan@users.noreply.github.com>
2025-09-11 23:03:40 -0700

    design
    implements navbar

---
+++ src/components/Hero.jsx
@@ -0,0 +1,46 @@
+import { useGSAP } from '@gsap/react';
+import { SplitText } from 'gsap/all';
+import gsap from 'gsap';
+
+const Hero = () => {
+    useGSAP(() =>{
+        const heroSplit = new SplitText('.title', {type: 'chars, words'});
+        const paragraphSplit = new SplitText('.subtitle', {type: 'lines'});
+
+    }, []);
+
+
+  return (
+    <>
+    <section id='hero' className='noisy'>
+        <h1 className='title'>LUVBUZZ</h1>
+        <img
+        src='/images/hero-left-leaf.png'
+        alt='left leaf'
+        className='left-leaf'
+        />
+        <img
+        src='/images/hero-right-leaf.png'
+        alt='right leaf'
+        className='right-leaf'
+        /> 
+
+        <div className='body'>
+            <div className='content'>
+                <div className='space-y-5 hidden md:block'>
+                    <p>Cool. Crips. Classic.</p>
+                    <p className='subtitle'>Sip the Spirit <br/> of Luvv</p>
+                </div>
+                <div className='view-cocktails'>
+                    <p className='subtitle'>Every drink on our menu is a blend of premium ingredients, creative flair, and timeless recipes - designed to delight your senses.
+                    </p>
+                    <a href='#cocktails'>View Menu</a>
+                </div>
+            </div>
+        </div>
+    </section>
+    </>
+  )
+}
+
+export default Hero
